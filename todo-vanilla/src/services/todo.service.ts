5a0ecc7fcea3c0f89c74d1bb477bf1af3691f1b1
Ekaterina <nikitinakatia0@gmail.com>
2023-09-26 19:41:33 +0300

    typed the data value

--- todo-vanilla/src/services/todo.service.ts
+++ todo-vanilla/src/services/todo.service.ts
@@ -1,17 +1,14 @@
 /** Сервис для взаимодействия с Todo: получения данных, обновление данных, удаление и т.п. */
 
-export async function fetchTodos() {
+export async function fetchTodos() : Promise<object> {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const data = await response.json();
+  const data : object = await response.json();
   return data
 }
 
-export async function fetchTodoById(id: number) {
+export async function fetchTodoById(id: number) : Promise<object> {
     const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-    const data = await response.json()
-    console.log(data);
-    
+    const data : object = await response.json()
     return data
-    
 }
 
