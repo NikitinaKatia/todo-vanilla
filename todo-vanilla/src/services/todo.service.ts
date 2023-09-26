5872e5e91c5bc5454db90b92c7d0ef8a2db2765c
Ekaterina <nikitinakatia0@gmail.com>
2023-09-26 22:20:01 +0300

    added Todo interface

--- todo-vanilla/src/services/todo.service.ts
+++ todo-vanilla/src/services/todo.service.ts
@@ -1,14 +1,21 @@
 /** Сервис для взаимодействия с Todo: получения данных, обновление данных, удаление и т.п. */
 
+interface Todo{
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean
+}
+
 export async function fetchTodos() : Promise<object> {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const data : object = await response.json();
+  const data : Todo = await response.json();
   return data
 }
 
 export async function fetchTodoById(id: number) : Promise<object> {
     const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-    const data : object = await response.json()
+    const data : Todo = await response.json()
     return data
 }
 
