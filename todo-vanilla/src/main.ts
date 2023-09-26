b1223bcceddb9b7e13b3316ca95196771f8789e2
Ekaterina <nikitinakatia0@gmail.com>
2023-09-26 18:46:38 +0300

    created basic functions

--- todo-vanilla/src/main.ts
+++ todo-vanilla/src/main.ts
@@ -1,8 +1,12 @@
 import "./theme/index.css";
+import { fetchTodos,fetchTodo } from './services/todo.service';
+import { TODOS_URL } from './core/constants'
 
 /** Initialization home page. */
 function initHomePage(): void {
   console.log("initHomePage");
+  fetchTodos(TODOS_URL)
+  fetchTodo(TODOS_URL + '/1')
 }
 
 window.addEventListener("DOMContentLoaded", initHomePage);
--- todo-vanilla/src/services/todo.service.ts
+++ todo-vanilla/src/services/todo.service.ts
@@ -1,1 +1,16 @@
 /** Сервис для взаимодействия с Todo: получения данных, обновление данных, удаление и т.п. */
+// const TODOS_URL: string = 'https://jsonplaceholder.typicode.com/todos/1';
+
+export async function fetchTodos(url: string) {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
+
+export async function fetchTodo(url: string) {
+    const response = await fetch(url);
+    const data = await response.json()
+    console.log(data);
+    
+}
+
