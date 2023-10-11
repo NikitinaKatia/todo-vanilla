e82e78b59c4702f89a10a51a956e63115392dfe5
Ekaterina <nikitinakatia0@gmail.com>
2023-10-11 08:36:56 +0300

    deleted an element from the array

--- todo-vanilla/src/ui/general.ts
+++ todo-vanilla/src/ui/general.ts
@@ -14,31 +14,53 @@ export function getDomElement<T extends Element>(
   throw new Error(`Element not found by selector ${selector}`);
 }
 
+
+function createDeleteBtn(listItem: HTMLLIElement, id: number) {
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'X';
+  
+  deleteBtn.addEventListener('click', () => {
+    const todoList = getDomElement('.todo-list');
+    todoList.removeChild(listItem);
+
+    todos.filter((todo, index) => {
+      if(id === todo.id) {
+        todos.splice(index, 1)
+      }
+    })
+  })
+  return deleteBtn
+}
+
+
 export function createListItem(text: string) {
   const listItem = document.createElement("li");
   const input = document.createElement("input");
   const span = document.createElement("span");
-  const deleteBtn = document.createElement('button');
-  deleteBtn.textContent = 'X'
-  input.type = "checkbox";
 
+  input.type = "checkbox";
+  
   const todoItem: Todo = {
     id: idTodo +=1,
     title: text,
     completed: input.checked,
   }
+  const deleteBtn = createDeleteBtn(listItem, todoItem.id)
+  
+  todos.push(todoItem);
 
   input.addEventListener('change', (event) => {
+    const target = event.target as HTMLInputElement
     
-    if((<HTMLInputElement>event.target).checked === true) {
+    if(target.checked === true) {
       span.style.textDecoration = 'line-through'
+      todoItem.completed = true
     }
 
-    if((<HTMLInputElement>event.target).checked === false) {
+    if(target.checked === false) {
       span.style.textDecoration = 'none'
+      todoItem.completed = false
     }
-    
-    todos.push(todoItem);
   })
   
   span.textContent = text;
@@ -54,16 +76,26 @@ export function createAddTodoClick() {
   const btnAddTodoElement = getDomElement('.add-todo');
   const input: HTMLInputElement = getDomElement('.addInput');
 
+  
+
   btnAddTodoElement.addEventListener('click', () => {
     const todoItemElement = createListItem(input.value)
-    
-    if(!input.value) {
+
+    if(input.value.trim() === '') {
       return
-    } else {
-      input.value.trim()
     }
     
     todoList.append(todoItemElement);
     input.value = '';
   })
+
+  input.addEventListener('change', () => {
+    const todoItemElement = createListItem(input.value)
+      if(input.value.trim() === '') {
+        return
+      }
+      todoList.append(todoItemElement);
+      input.value = '';
+  })
+  
 }
